13c15f1ede966db5734e35cbf1c1f6e0f56616e9
demo <dazanderofficials@gmail.com>
2024-04-18 21:28:16 +0800

    base init

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+def render(template : str):
+  return render_template(f"{template}.html")
+
+def redirect_to(template_name : str):
+  return redirect(url_for(template_name))
+
+@app.route('/')
+def index():
+  return render("home")
+
+@app.route('/home')
+def home():
+  return redirect_to("/")
+
+@app.route('/id-one')
+def id_one():
+  return render("home")
+
+@app.route('/id-two')
+def id_two():
+  return render("home")
+
+@app.route('/id-three')
+def id_three():
+  return render("home")
+
+@app.route('/about-us')
+def about_us():
+  return render("about_us")
+
+@app.route('/redirect')
+def redirect_page():
+  return redirect_to(request.args.get("data"))
+
+if __name__ == "__main__":
+  app.run()
+
+class_ = "bg-pearl child-full border-radius-8"
